a48ff1dd1086c9c31bd87c254b2157d3a208457a
Ty Kowalewski <tykowale@gmail.com>
2015-03-27 16:57:40 -0500

    Updated login and authentication methods

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,3 +1,21 @@
 class AuthController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def login
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+    else
+      flash[:alert] = "You entered in the wrong username or password"
+    end
+  end
+
+  def logout
+    session.clear
+    redirect_to root_path
+  end
+
 end
