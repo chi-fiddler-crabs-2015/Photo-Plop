f132caa2a23cd873230d9825cd512922fb163e60
Stephanie Marx <stephanie.n.marx@gmail.com>
2015-03-27 16:44:37 -0500

    Update users controller and create view for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to albums_path
+    else
+      @errors = @user.errors
+      render :'new'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
