4c11b2cb69a8d6385f061f55667ea5b16efa06e4
Kara A Carrell <karaAJC@gmail.com>
2015-03-27 18:38:12 -0500

    updating collaborators albums model spec

--- spec/models/collaborators_albums_model_spec.rb
+++ spec/models/collaborators_albums_model_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe CollaboratorsAlbum, type: :model do
 
+  it { should belong_to(:album) }
+
   it { should belong_to(:collaborator) }
 
-  it { should belong_to(:album) }
+  it { should have_db_index(:album_id) }
+
+  it { should have_db_index(:collaborator_id) }
 
 end
