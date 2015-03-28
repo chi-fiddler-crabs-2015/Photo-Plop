70a93688a1c922cc41bfb232ecaf524eecb309e0
Kara A Carrell <karaAJC@gmail.com>
2015-03-27 19:37:50 -0500

    updating image model specs

--- spec/models/images_model_spec.rb
+++ spec/models/images_model_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
+  describe "Associations" do
 
-  it { should belong_to(:owner) }
+    it { should belong_to(:owner) }
+
+    it { should belong_to(:album) }
+
+  end
+
+  describe "Validations" do
+
+    it { should validate_presence_of(:url) }
+
+  end
+
+  describe "Database" do
+
+    it { should have_db_index(:album_id) }
+
+    it { should have_db_index(:owner_id) }
+
+  end
 
 
 end
