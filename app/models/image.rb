92e795b662eb1ebaf926eb5c98fe066dddd987ab
Ty Kowalewski <tykowale@gmail.com>
2015-03-27 19:26:19 -0500

    Fixed error in live updates for adding images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
-lass Image < ActiveRecord::Base
+class Image < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
   belongs_to :album
 
@@ -8,7 +8,7 @@ lass Image < ActiveRecord::Base
   # we get url from uploading the image after the instance of the model is created
 
   def notify_album
-    self.album.connection.execute "NOTIFY albums, 'data'"
+    Album.connection.execute "NOTIFY albums, 'data'"
   end
 
 end
