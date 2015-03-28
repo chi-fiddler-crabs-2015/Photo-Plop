6e2ab1d770c90a18254bd013176921f5d8d52dcc
Kara A Carrell <karaAJC@gmail.com>
2015-03-27 19:43:07 -0500

    updating spec with context

--- spec/models/collaborators_albums_model_spec.rb
+++ spec/models/collaborators_albums_model_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CollaboratorsAlbum, type: :model do
+  describe "Associations" do
 
-  it { should belong_to(:album) }
+    it { should belong_to(:album) }
 
-  it { should belong_to(:collaborator) }
+    it { should belong_to(:collaborator) }
 
-  it { should have_db_index(:album_id) }
+  end
 
-  it { should have_db_index(:collaborator_id) }
+  describe "Database" do
+    it { should have_db_index(:album_id) }
 
+    it { should have_db_index(:collaborator_id) }
+  end
 end
--- spec/models/users_model_spec.rb
+++ spec/models/users_model_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "Associations" do
 
-  it { should have_many(:albums) }
+    it { should have_many(:albums) }
 
-  it { should have_many(:collaborators_albums) }
+    it { should have_many(:collaborators_albums) }
 
-  it { should have_many(:images) }
+    it { should have_many(:images) }
+  end
 
-  it { should validate_presence_of(:email) }
-
-  it { should validate_presence_of(:password_hash) }
-  describe "uniqueness" do
+  describe "Validations" do
     subject { FactoryGirl.build(:user) }
+
+    it { should validate_presence_of(:email) }
+
+    it { should validate_presence_of(:password_hash) }
 
     it { should validate_uniqueness_of(:email) }
 
