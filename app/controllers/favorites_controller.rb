2bee28b30b268b4b80b9b82d2b2cb0fce9df1434
Tyler Kowalewski <tykowale@gmail.com>
2015-03-29 16:08:18 -0500

    Updated favorite button functionality

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,17 @@ class FavoritesController < ApplicationController
   def create
     fav = Favorite.find_or_create_by(user_id: params[:user], album_id: params[:album])
     fav.change_status
+    fav.save
+    puts fav.favorite
+    puts "*************************"
+    redirect_to :back
     # redirect_to albums_path(params[:album])
   end
 
-  def destroy
-    Favorite.find(params[:id]).destroy
-    redirect_to albums_path
-  end
+  # def destroy
+  #   Favorite.find(params[:id]).destroy
+  #   redirect_to albums_path
+  # end
 
 
 end
