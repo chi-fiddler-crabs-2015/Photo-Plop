161f4b901066c2aa8d5795e6f95ee6ddb3c3249b
Ty Kowalewski <tykowale@gmail.com>
2015-03-30 15:57:46 -0500

    Image controller specs

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -2,22 +2,48 @@ require 'rails_helper'
 
 RSpec.describe ImagesController, type: :controller do
   let(:user) { create(:user) }
-  let(:album) { create(:album, creator: user) }
+  let(:album) { create(:album, creator: user, vanity_url: 'asldkfj234') }
+  let(:image) { create(:image, owner: user, album: album) }
   before do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
   context 'GET #new' do
     it 'assigns @image to a new image' do
-      get :new, {album_id: album.id}
+      get :new, { album_id: album.id }
       expect(assigns(:image)).to be_a_new Image
     end
 
     it 'belongs to an album' do
-      get :new, {album_id: album.id}
+      get :new, { album_id: album.id }
       expect(assigns(:album)).to be_a Album
+    end
+
+    it 'renders new partial if write privilege 1' do
+      get :new, { album_id: album.id }
+      expect(response).to render_template("images/_new")
+    end
+
+    it 'asks for a password if write privilege is > 1' do
+      album = create(:album, write_privilege: 2)
+      get :new, { album_id: album.id }
+      expect(response).to render_template("images/_prompt_for_password")
     end
   end
 
+  context 'GET #show' do
+    it 'assigns @image to current image' do
+      get :show, { album_id: album.id, id: image.id}
+      expect(assigns(:image)).to be_a Image
+    end
+  end
 
+  context 'DELETE #destroy' do
+    it 'destroys an image' do
+      image = create(:image, owner: user)
+      expect {
+        delete :destroy, { album_id: album.id, id: image.id }
+      }.to change{Image.count}
+    end
+  end
 end
--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :image do
     caption FFaker::Company.catch_phrase
-    url "1234567890abcdefg".split('').shuffle.join
+    location "1234567890abcdefg".split('').shuffle.join
     association :owner, factory: :user
     association :album, factory: :album
   end
