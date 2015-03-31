a8eb6943a6baa6bb37714c8215eee4e0da99042e
Ty Kowalewski <tykowale@gmail.com>
2015-03-30 19:00:57 -0500

    Start tests for application controller

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  context 'checks if a user is logged in' do
+    it 'redirects to login path is user is not logged in' do
+      expect(get :index).to redirect_to login_path
+    end
+
+    it 'redirects to album path if user is logged in' do
+      allow_any_instance_of(ApplicationController).to receive(:logged_in?).and_return(true)
+      expect(get :index).to redirect_to albums_path
+    end
+  end
+
+end
