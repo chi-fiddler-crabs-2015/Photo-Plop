eb382047c0af5047e34948ed319f44063df0fdd3
Tyler Kowalewski <tykowale@gmail.com>
2015-03-31 15:54:52 -0500

    Fixed hide button

--- app/assets/javascripts/views/new-picture-upload.js
+++ app/assets/javascripts/views/new-picture-upload.js
@@ -21,20 +21,14 @@ $(document).ready(function() {
 
   $(document).on('submit', '#image-password-form', function(e) {
     form = $(this);
-    if(showing) {
-      $('#to_append_to').html('')
-      showing = false
-    } else {
-      $.ajax({
-        url: form.attr('action'),
-        method: form.attr('method'),
-        data: form.serialize(),
-        success: function(response) {
-          $('#to_append_to').html(response);
-        }
-      })
-      showing = true
-    }
+    $.ajax({
+      url: form.attr('action'),
+      method: form.attr('method'),
+      data: form.serialize(),
+      success: function(response) {
+        $('#to_append_to').html(response);
+      }
+    })
   })
 
 })
