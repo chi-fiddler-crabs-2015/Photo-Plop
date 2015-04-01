50b6d9721686e2e5703539ef646a122a8f3c3b12
karaAJC <KaraAJC@gmail.com>
2015-03-31 23:16:36 -0500

    got tags results for instagram

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -45,18 +45,16 @@ class ImagesController < ApplicationController
     redirect_to album_path(params[:album_id])
   end
 
-def tags
-  puts params
-  client = Instagram.client(:access_token => ENV['IG_APP_SECRET'])
-  html = "<h1>Search for tags, get tag info and get media by tag</h1>"
-  tags = client.tag_search('cat')
-  html << "<h2>Tag Name = #{tags[0].name}. Media Count =  #{tags[0].media_count}. </h2><br/><br/>"
-  for media_item in client.tag_recent_media(tags[0].name)
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
+  def tags
+    client = Instagram.client(:access_token => ENV['IG_ACCESS_TOKEN'])
+    @html = "<h1>Instagram pics with your album's Tag:</h1>"
+    tags = client.tag_search(params[:tag])
+    @html << "<h2>Tag Name = #{tags[0].name}. Pic Count =  #{tags[0].media_count}. </h2><br/><br/>"
+    for media_item in client.tag_recent_media(tags[0].name)
+      @html << "<img src='#{media_item.images.thumbnail.url}'>"
+    end
+    @html
   end
-  puts html
-end
-
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
 
   resources :collaborators_albums
   resources :favorites
-  get '/search' => 'images#tags'
+  get '/search/:tag' => 'images#tags', as: :search
 
   get'/:vanity_url' => 'albums#vanity', as: :vanity
   # Example of regular route:
